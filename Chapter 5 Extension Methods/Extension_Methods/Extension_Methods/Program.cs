fbff928702d6b4da5f0b8b2666425cdc83366dc8
George Tourtsinakis <george@besmartbesimple.com>
2017-12-27 16:09:55 +0200

    Extension methods
     - Explaination of extension methods

---
+++ Chapter 5 Extension Methods/Extension_Methods/Extension_Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string post = "This is supposed to be a very long text This is supposed to be a very long text This is supposed to be a very long text This is supposed to be a very long text This is supposed to be a very long textThis is supposed to be a very long text  ";
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+
+        }
+    }
+
+   
+
+    
+}
---
+++ Chapter 5 Extension Methods/Extension_Methods/Extension_Methods/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods
+{
+    public static class StringExtensions
+    {
+        //Shorten string according to number of  words
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("Number of words should not be less than 0");
+
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+            {
+                return str;
+            }
+            return string.Join(" ", words.Take(numberOfWords));
+
+        }
+
+    }
+
+}
