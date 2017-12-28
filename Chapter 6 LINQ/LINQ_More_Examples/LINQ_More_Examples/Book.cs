b14fd68766840fa3dfe9517241b0638207325c89
George Tourtsinakis <george@besmartbesimple.com>
2017-12-28 16:34:06 +0200

    LINQ finished with more examples and extension methods

---
+++ Chapter 6 LINQ/LINQ_More_Examples/LINQ_More_Examples/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace LINQ_More_Examples
+{
+    public class Book
+    {
+        public float Price { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Chapter 6 LINQ/LINQ_More_Examples/LINQ_More_Examples/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_More_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().Getbooks();
+            var singleTitle = books.SingleOrDefault(b => b.Title == "ASP.NET MVC Step by Step");
+
+            if (singleTitle != null)
+            {
+                Console.WriteLine(singleTitle.Title);
+                Console.WriteLine(singleTitle.Price);
+            }
+            else
+                throw new InvalidOperationException("We have empty result from query");
+
+            //Return firstResult
+            var firstResult = books.First(b => b.Title == "C# Advanced Topics");
+            Console.WriteLine(firstResult.Title);
+
+            //Skip first two records and return total 2
+            var pagedBooks = books.Skip(2).Take(2);
+
+            foreach (var book in pagedBooks)
+            {
+                Console.WriteLine(book.Title);
+                Console.WriteLine(book.Price);
+            }
+
+            //Return how many books
+            var totalBooks = books.Count();
+            Console.WriteLine("Total books :" + totalBooks);
+
+            var highestPrice = books.Max(b => b.Price);
+            Console.WriteLine("The highest price is " + highestPrice);
+
+        }
+
+    }
+}
