e6f68a2e3f9e482b3b12d921fe4ad13901ffd461
George Tourtsinakis <george@besmartbesimple.com>
2017-12-28 15:05:47 +0200

    LINQ
    - LINQ syntax and examples
    - LINK chaining

---
+++ Chapter 6 LINQ/LINQ/LINQ/BookRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class BookRepository
+    {
+        //Hardcoded books
+        public IEnumerable<Book>Getbooks()
+        {
+            return new List<Book>
+            {
+                new Book() { Title = "ADO.NET Step by Step",Price = 5},
+                new Book() { Title = "ASP.NET MVC Step by Step", Price = 9.99f},
+                new Book() { Title = "ASP.NET Web API",Price = 12},
+                new Book() { Title = "C# Advanced Topics",Price = 7}
+            };
+        }
+
+
+    }
+}
---
+++ Chapter 6 LINQ/LINQ/LINQ/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().Getbooks();
+
+            foreach (var book in books)
+            {
+                Console.WriteLine("Book Title :" + book.Title);
+                Console.WriteLine("Price :" + book.Price +"$");
+            }
+
+            //Display books less than 10$ without LINQ
+            var cheapBooks = new List<Book>();
+            foreach (var book in books)
+            {
+                if (book.Price < 10 )
+                {
+                    cheapBooks.Add(book);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***BOOKS UNDER 10$***");
+
+            foreach (var cheapBook in cheapBooks)
+            {
+                
+                Console.WriteLine("Book Title :" + cheapBook.Title);
+                Console.WriteLine("Price :" + cheapBook.Price + "$");
+            }
+
+            //With LINQ Extension Methods
+            //Books under 10,Order By Price and show only Title
+            var cheapBooksLINQ = books
+                                    .Where(b => b.Price < 10)
+                                    .OrderBy(b => b.Price)
+                                    .Select(b => b.Title); 
+
+            Console.WriteLine();
+            Console.WriteLine("***BOOKS UNDER 10$ with LINQ***");
+
+            foreach (var cheapBook in cheapBooksLINQ)
+            {
+                Console.WriteLine("Book Title :" + cheapBook);
+                
+            }
+
+            //With LINQ Query Operators
+            var cheaperBooksLINQ = from b in books
+                                   where b.Price < 10
+                                   orderby b.Price
+                                   select b;
+
+            Console.WriteLine();
+            Console.WriteLine("***BOOKS UNDER 10$ with LINQ***");
+
+            foreach (var cheaperBook in cheaperBooksLINQ)
+            {
+                Console.WriteLine("Book Title :" + cheaperBook.Title);
+                Console.WriteLine("Price :" + cheaperBook.Price);
+            }
+
+
+        }
+    }
+}
