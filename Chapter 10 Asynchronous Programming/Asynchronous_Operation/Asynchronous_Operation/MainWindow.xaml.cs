fb986b3b8a3580a34beea6aa8f27be6e7aabecbd
George Tourtsinakis <george@besmartbesimple.com>
2018-01-05 13:17:10 +0200

    Finished with Asynchronous programming
    - Added Asynchronous operations/programming
    - async/await keywords explenation

---
+++ Chapter 10 Asynchronous Programming/Asynchronous_Operation/Asynchronous_Operation/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asynchronous_Operation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Top button
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadHtmlAsync("https://msdn.microsoft.com/en-us/");
+        }
+
+
+
+        //Second button
+        private async void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var getHtmlTask = GetHtmlAsync("https://msdn.microsoft.com/en-us/");
+
+            //Do something while await method runs
+            MessageBox.Show("I m running while the await method is executed");
+
+            var html = await getHtmlTask;
+            MessageBox.Show(html.Substring(0,10));
+
+        }
+
+
+        public async Task<string> GetHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+
+            return await webClient.DownloadStringTaskAsync(url);
+        }
+
+        public async Task DownloadHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+            var html = await webClient.DownloadStringTaskAsync(url);//Download as string the url 
+
+            try
+            {
+                using (var streamWriter = new StreamWriter(@"C:/Users/Giant/Documents/Visual Studio 2015/Projects/C-Sharp-Advanced/Chapter 10 Asynchronous Programming/result.html"))
+                {
+                    await streamWriter.WriteAsync(html);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("A handled exception just occurred: " + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+       
+    }
+}
---
+++ Chapter 10 Asynchronous Programming/Asynchronous_Programming/Asynchronous_Programming/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asynchronous_Programming
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    // **** Without Asynchronous operation ****
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadHtml("https://msdn.microsoft.com/en-us/");
+        }
+
+
+        public void DownloadHtml(string url)
+        {
+            var webClient = new WebClient();
+            var html = webClient.DownloadString(url);//Download as string the url 
+
+            try
+            {
+                using (var streamWriter = new StreamWriter(@"C:/Users/Giant/Documents/Visual Studio 2015/Projects/C-Sharp-Advanced/Chapter 10 Asynchronous Programming/result.html"))
+                {
+                    streamWriter.Write(html);
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("A handled exception just occurred: " + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+
+        }
+    }
+}
